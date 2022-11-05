b90f028ee96b1e2f83920a46c65a34c715a64d3c
React <>
2022-11-04 21:53:30 -0400

    added the candy divisions function

---
+++ candy_division.js
@@ -0,0 +1,11 @@
+const candy_division = (n) =>{
+    let numOfFriends = []
+    for(let x=0;x<=n;x++){        
+        if((n / (x+1)) % 1 === 0){
+            numOfFriends.push(x);
+        }
+    }
+return numOfFriends;
+}
+
+console.log(candy_division(25))
